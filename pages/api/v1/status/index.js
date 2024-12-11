f01911e9d5f3190c1a9a3e23ed76ece9a83f3e42
unknown <guilhermedeoliveiradesouza@gmail.com>
2024-12-11 11:23:59 -0300

    finish api/v1/status endoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,48 @@
 import database from "@/infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1;");
-  console.log(result.rows[0]);
-  response.status(200).json({ message: "Deu certo meu consagradão!" });
+  const updatedAt = new Date().toISOString();
+
+  try {
+    const databaseVersionResult = await database.query({
+      text: "SHOW server_version;",
+    });
+
+    const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+    const databaseMaxConnectionsResult = await database.query({
+      text: "SHOW max_connections;",
+    });
+
+    const databaseMaxConnectionsValue =
+      databaseMaxConnectionsResult.rows[0].max_connections;
+
+    const databaseName = process.env.POSTGRES_DB;
+
+    const databaseOpenedConnectionsResult = await database.query({
+      text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+      values: [databaseName],
+    });
+
+    const databaseOpenedConnectionsValue =
+      databaseOpenedConnectionsResult.rows[0].count;
+
+    response.status(200).json({
+      updated_at: updatedAt,
+      dependencies: {
+        database: {
+          version: Number(databaseVersionValue),
+          max_connections: parseInt(databaseMaxConnectionsValue),
+          opened_connections: databaseOpenedConnectionsValue,
+        },
+      },
+    });
+  } catch (error) {
+    console.error("Erro ao obter informações do banco de dados:", error);
+    response
+      .status(500)
+      .json({ error: "Erro ao obter informações do banco de dados" });
+  }
 }
 
 export default status;
